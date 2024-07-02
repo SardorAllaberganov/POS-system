670e89ddf1afdeb6d5ae33518b032092a41b02ec
SardorAllaberganov <sardorallaberganov96@gmail.com>
2024-07-02 17:18:05 +0500

    user model created

---
+++ model/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "cashier", "manager", "customer"],
+        default: "customer",
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
