5424395082f0391f35b474c4380190b4fff69b16
SardorAllaberganov <sardorallaberganov96@gmail.com>
2024-07-04 16:41:36 +0500

    some files added and sales schema created

---
+++ model/sales.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const salesSchema = mongoose.Schema({
+    products: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    cashier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Sales", salesSchema);
