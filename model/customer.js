b823340b2abd4b2dfcdbdf3e129bee482519d793
SardorAllaberganov <sardorallaberganov96@gmail.com>
2024-07-18 09:11:33 +0500

    some changes in customer model

--- model/customer.js
+++ model/customer.js
@@ -1,40 +1,38 @@
 const mongoose = require("mongoose");
 
 const addressSchema = mongoose.Schema({
-	street: String,
-	city: String,
-	state: String,
-	country: String,
-	postalCode: String,
+    street: String,
+    city: String,
+    state: String,
+    country: String,
+    postalCode: String,
 });
 
 const customerSchema = mongoose.Schema(
-	{
-		firstName: {
-			type: String,
-			required: true,
-		},
-		lastName: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		phone: {
-			type: String,
-			required: true,
-		},
-		addresses: [addressSchema],
-		loyaltyPoints: {
-			type: Number,
-			default: 0,
-		},
-	},
-	{
-		timestamps: true,
-	}
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: String,
+        dob: Date,
+        phone: {
+            type: String,
+            required: true,
+        },
+        addresses: [addressSchema],
+        loyaltyPoints: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model("Customer", customerSchema);
